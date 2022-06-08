19cccb1f7e99ec43426894ce314e1cba8563cb43
Daniela Onici <danielaonici@gmail.com>
2022-06-08 11:34:23 -0400

    new line

--- script.js
+++ script.js
@@ -5,19 +5,25 @@
 // Daniela Onici Student ID# 8754297, 2022.06.07: Created
 //                                    2022.06.08: Finished
 
-function itemQuantity() {
-  var itemQuantity = parseInt(prompt("Add the quantity"));
+function addingRedVelvet() {
+  var quantity = parseInt(prompt("Add the quantity"));
+  const cart = [];
 
-  if (isNaN(redVelvet)) {
+  if (isNaN(quantity)) {
     alert("Not a valid number. Please insert only numbers");
-  } else if (redVelvet <= 0) {
+  } else if (quantity <= 0) {
     alert("Invalid number. Please insert a number greater than 0");
   } else {
-    const cart = [];
-    var qttRedVelvet = 45 * redVelvet;
+    var qttRedVelvet = 45 * quantity;
     cart.includes(qttRedVelvet);
-    alert(qttRedVelvet);
   }
 }
 
-
+function receipt(cart) {
+  var name = prompt("Please, insert your name:");
+  if (isFinite(name)) {
+    alert("Invalid name. Please, input a name");
+  } else if (name == null) {
+    alert("Empty. Please insert your name.");
+  }
+}
